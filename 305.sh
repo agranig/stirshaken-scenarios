12b6c58f5e097317d6a02b38544a0a3a9a8aa8b4
Andreas Granig <andreas@granig.com>
2021-04-14 10:49:32 +0000

    Fix hardcoded paths to helpers and certs

--- 305.sh
+++ 305.sh
@@ -11,4 +11,4 @@ fi
 SIPP="$BD/helpers/sipp"
 RUNNER="$BD/helpers/run-invite.sh"
 
-"$RUNNER" "$SIPP" "$S" "$T" "$BD/helpers/create-jwt.pl --keypath=$BD/helpers/certs/sp/priv.pem --autoiat --autoorigid --origtn=SF_EMPTY = desttn=439991002 --x5u=http://sipp.opensipit.sipfront.org/cert.pem --attest=B"
+"$RUNNER" "$SIPP" "$S" "$T" "$BD/helpers/create-jwt.pl --keypath=$BD/certs/sp/priv.pem --autoiat --autoorigid --origtn=SF_EMPTY = desttn=439991002 --x5u=http://sipp.opensipit.sipfront.org/cert.pem --attest=B"
